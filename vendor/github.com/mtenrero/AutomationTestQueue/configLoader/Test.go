97a068fb1f2d55f6666ed43d1b06b87b6a37389f
Marcos Tenrero <tenrero@aol.com>
2018-03-24 19:25:06 +0100

    Removed dep vendor directory

--- vendor/github.com/mtenrero/AutomationTestQueue/configLoader/Test.go
+++
@@ -1,8 +0,0 @@
-package configLoader
-
-// Test defines the test file and the tool/TestType which will be launched with
-type Test struct {
-	Tool *Tool             `json:"tool"`
-	Name string            `json:"name"`
-	Envs map[string]string `json:"envs"`
-}
