c8c77eb3d75f7c94e1e8a9c21f29b36e5eda3f81
Marcos Tenrero <marcos.95tm@gmail.com>
2018-03-27 21:47:51 +0200

    Added funcs for Swarm Basic Management

---
+++ dockerInterpreter/swarm.go
@@ -0,0 +1,25 @@
+package dockerInterpreter
+
+import (
+	"context"
+
+	"docker.io/go-docker/api/types/swarm"
+)
+
+// SwarmInspect gives the information about the Swarm
+func SwarmInspect() (swarm.Swarm, error) {
+	client := getClient()
+
+	return client.SwarmInspect(context.Background())
+}
+
+// IsSwarmNode returns an error if the node is not a swarm node member
+func IsSwarmNode() bool {
+	client := getClient()
+	_, error := client.SwarmInspect(context.Background())
+
+	if error == nil {
+		return true
+	}
+	return false
+}
---
+++ dockerInterpreter/swarm_test.go
@@ -0,0 +1,53 @@
+package dockerInterpreter
+
+import (
+	"context"
+	"testing"
+
+	"docker.io/go-docker/api/types/swarm"
+)
+
+func TestSwarmMember(t *testing.T) {
+
+	memberBefore := IsSwarmNode()
+
+	client := getClient()
+
+	if !memberBefore {
+		_, error := client.SwarmInit(context.Background(), swarm.InitRequest{
+			ListenAddr: "0.0.0.0",
+		})
+		t.Log(error)
+	}
+
+	if !IsSwarmNode() {
+		t.Error("NOT MEMBER OF A SWARM CLUSTER!")
+	}
+
+	if !memberBefore {
+		client.SwarmLeave(context.Background(), false)
+	}
+}
+
+func TestSwarmInspect(t *testing.T) {
+	memberBefore := IsSwarmNode()
+
+	client := getClient()
+
+	if !memberBefore {
+		_, error := client.SwarmInit(context.Background(), swarm.InitRequest{
+			ListenAddr: "0.0.0.0",
+		})
+		t.Log(error)
+	}
+
+	_, error := SwarmInspect()
+
+	if error != nil {
+		t.Error(error)
+	}
+
+	if !memberBefore {
+		client.SwarmLeave(context.Background(), false)
+	}
+}
