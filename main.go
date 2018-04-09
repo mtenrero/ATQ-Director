95689e20ec88add9e4044b407b3c973cad86aca3
Marcos Tenrero <marcos.95tm@gmail.com>
2018-04-09 20:39:29 +0200

    Config load on program start

--- main.go
+++ main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"os"
 
+	"github.com/mtenrero/ATQ-Director/configLoader"
 	"github.com/mtenrero/ATQ-Director/dockerMiddleware"
 	"github.com/sirupsen/logrus"
 )
 
+// LOGGER is the Logrus logger global instance
 var LOGGER *logrus.Logger
+
+// CONFIG holds the application configuration
+var CONFIG configLoader.ControllerConfig
 
 func init() {
 	logrus.SetOutput(os.Stdout)
 	LOGGER = logrus.New()
-
 }
 
 func main() {
+	CONFIG, err := configLoader.LoadControllerConfigYaml("controller-config.yaml")
+	if err != nil {
+		LOGGER.Fatal(err)
+	}
+
+	LOGGER.Info(CONFIG.Port)
+
 	swarmChecks()
 }
 
